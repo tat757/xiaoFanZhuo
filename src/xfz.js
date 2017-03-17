43e596fa17d07c6e21baca9aa5998376054cc72b
Yinsong Xu <xuyins@oregonstate.edu>
2017-03-16 18:25:24 -0700

    0.4.2 -添加timeline的结构 | Adding the layout of timeline

--- src/xfz.js
+++ src/xfz.js
@@ -6,9 +6,11 @@ var xfz = {
 		currUser : {
 			unique_id : ''
 		},
-		nav : 'home'
+		nav : 'home',
+		notMain : false,
 	},
 	init : function(){
+		document.body.style.overflowY = 'hidden';
 		xfz.status.page = 'welcome';
 		xfz.setPage();
 	},
@@ -68,30 +70,26 @@ var xfz = {
 		var inputAreaStyle = {
 			boxSizing : 'border-box',
 			width : '310px',
-			paddingLeft : '5px',
-			paddingRight : '5px',
-			resize : 'none',
-			display : 'inline-block'
+			resize : 'none'
 		};
 		xfz.setStyle(inputArea, inputAreaStyle);
 		var inputContainerStyle = {
 			paddingLeft : '5px',
 			paddingRight : '5px',
-			paddingBotton : '0px'
+			paddingBotton : '0px',
+			height : '65px'
 		};
 		xfz.setStyle(inputContainer, inputContainerStyle);
 		var postBtStyle = {
-			float : 'right',
 			paddingRight : '5px',
 			marginTop: '-5px'
 		};
 		xfz.setStyle(postBt, postBtStyle);
 		var currUserAvatarStyle = {
 			width : '32px',
-			paddingRight : '4px',
-			paddingLeft : '4px',
-			paddingBottom : '6px',
-			display : 'inline-block'
+			height : '32px',
+			paddingRight : '3px',
+			float : 'left'
 		};
 		xfz.setStyle(currUserAvatar, currUserAvatarStyle);
 		postBt.innerHTML = '发布';
@@ -108,7 +106,134 @@ var xfz = {
 		return inputContainer;
 	},
 	setBody : function(){
+		switch(xfz.status.nav){
+			case 'timeline' : 
+				xfz.body.timeline();
+				break;
+			case 'mention' :
+				xfz.body.mention();
+				break;
+			case 'message' :
+				xfz.body.message();
+				break;
+			case 'favorite' :
+				xfz.body.favorite();
+				break;
+		}
+	},
+	body : {
+		timeline : function(){
+			document.getElementById('bodyContainer').innerHTML = '载入中..';
+			if(xfz.status.notMain){
+				bodyContainer.innerHTML = 'Under construction';
+			}
+			else{
+				var timeline = document.createElement('div');
+				timeline.id = 'timeline';
+				var timelineStyle = {
+					height: '550px',
+					overflowY : 'scroll'
+				};
+				xfz.setStyle(timeline, timelineStyle);
+				var timelineNotification = document.createElement('div');
+				timelineNotification.id = 'timelineNotification';
+				var timelineStream = document.createElement('div');
+				timelineStream.id = 'timelineStream';
+				var ol = document.createElement('ol');
+				timelineStream.appendChild(ol);
 
+				xfz.appendChilds(timeline, [timelineNotification, timelineStream]);
+				xfz.Get('/action/getCurrUserHomeTimeline', function(data){
+					if(!data.data){
+						var li = document.createElement('li');
+						li.style.listStyleType = 'none';
+						li.innerHTML = '没有信息哦.';
+						ol.appendChild(li);
+					}
+					else{
+						bodyContainer.innerHTML = '';
+						bodyContainer.appendChild(timeline);
+						for(count in data.data){
+							var status = data.data[count];
+							console.log('status');
+							console.log(status);
+							var li = document.createElement('li');
+							var liStyle = {
+								width : '320px',
+								minHeight : '60px',
+								listStyleType : 'none'
+							};
+							xfz.setStyle(li, liStyle);
+							var userAvatarCell = document.createElement('a');
+							var userAvatarCellStyle = {
+								display : 'inline-block',
+								width : '12%',
+								height : '32px',
+								float : 'left',
+								marginLeft : '-35px'
+							};
+							xfz.setStyle(userAvatarCell, userAvatarCellStyle);
+							var userAvatar = document.createElement('img');
+							var userAvatarStyle = {
+								width: '32px',
+								height : '32px',
+								fontSize: '12px',
+							};
+							xfz.setStyle(userAvatar, userAvatarStyle);
+							userAvatar.src = status.user.profile_image_url;
+							userAvatarCell.appendChild(userAvatar);
+							var content = document.createElement('div');
+							var contentStyle = {
+								width: '83%',
+								display : 'inline-block'
+							}
+							xfz.setStyle(content, contentStyle);
+							var contentTop = document.createElement('a');
+							contentTop.innerHTML = status.user.name;
+							var contentTopStyle = {
+								display: 'block',
+								fontSize : '13px',
+							}
+							xfz.setStyle(contentTop, contentTopStyle);
+							var contentBottom = document.createElement('span');
+							var contentBottomStyle = {
+								display: 'block',
+								fontSize: '12px',
+							}
+							xfz.setStyle(contentBottom, contentBottomStyle);
+							contentBottom.innerHTML = status.text;
+							xfz.appendChilds(content, [contentTop, contentBottom]);
+							var controlPanel = document.createElement('span');
+							var controlPanelStyle = {
+								display: 'inline-block',
+								width: '5%',
+								height: '60px',
+								float: 'right',
+								marginRight : '20px'
+							}
+							var reply = document.createElement('a');
+							reply.innerHTML = 'R';
+							var replyStyle = {
+								position : 'absolute',
+								zIndex : '1',
+								right : '5px'
+							}
+							controlPanel.appendChild(reply);
+							xfz.setStyle(controlPanel, controlPanelStyle);
+							xfz.appendChilds(li, [userAvatarCell, content, controlPanel]);
+							var hr = document.createElement('hr');
+							var hrStyle = {
+								width : '360px',
+								marginLeft : '-40px',
+							}
+							xfz.setStyle(hr, hrStyle);
+							li.appendChild(hr);
+							ol.appendChild(li);
+						}
+					}
+				});
+			}
+		}
 	},
 	setStyle : function(id, styles){
 		for(style in styles){
@@ -123,6 +248,10 @@ var xfz = {
 	page : {
 		welcome : function(){
 			container.innerHTML = '请稍候..';
+			var containerStyle = {
+				overflow : 'hidden'
+			}
+			xfz.setStyle(container, containerStyle);
 			xfz.Get('/action/authorize', function(data){
 				xfz.status.page = 'main';
 				xfz.setPage();
@@ -163,6 +292,7 @@ var xfz = {
 			navBarContainer.appendChild(timelineBar);
 
 			var bodyContainer = document.createElement('div');
+			bodyContainer.id = 'bodyContainer';
 			var timeline = document.createElement('div');
 			
 
@@ -173,15 +303,15 @@ var xfz = {
 					console.log(inputContainer);
 					xfz.status.currUser = data.data;
 					container.innerHTML = '';
+					xfz.appendChilds(div, [inputContainer, navBarContainer, bodyContainer]);
+					container.appendChild(div);
 					xfz.Get('/action/getCurrAvatar', function(img){
 						console.log(img);
 						document.getElementById('currUserAvatar').src = 'data:image/jpeg;base64,' + img.data.image;
 					});
-					xfz.Get('/action/getCurrUserHomeTimeline', function(userStatuses){
-
-					});
-					xfz.appendChilds(div, [inputContainer, navBarContainer]);
-					container.appendChild(div);
+					xfz.status.nav = 'timeline';
+					xfz.status.notCurrUser = false;
+					xfz.setBody();
 					console.log(document.getElementById('currUserAvatar'));
 				}
 			});
